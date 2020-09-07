ac28c5f80df71e23c98b4974fd7c39fca10af4f7
pashaapsky <ap.sky@yandex.ru>
2020-09-07 22:43:01 +0300

    make tags cloud for posts

---
+++ app/Tag.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    protected $table = 'tags';
+
+    protected $guarded = [];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+}
